b2bdf7752e491df9c6c1d0c3560095f4e632ccd5
Asael211146 <91224136+Asael211146@users.noreply.github.com>
2025-11-11 02:49:10 -0600

    Extend Editor class from Usuario and add methods
    The Editor class now extends the Usuario class and includes methods for creating and editing content.

--- src/model/Editor.java
+++ src/model/Editor.java
@@ -1,3 +1,16 @@
-public class Editor {
-    
+public class Editor extends Usuario {
+
+    // Constructor
+    public Editor(String nombre, String email) {
+        super(nombre, email);
+    }
+
+    // Métodos
+    public void crearContenido(Contenido c) {
+        System.out.println("El editor creó el contenido: " + c._getTitulo());
+    }
+
+    public void editarContenido(Contenido c) {
+        System.out.println("El editor editó el contenido: " + c._getTitulo());
+    }
 }
