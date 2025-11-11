085c69ba8eab7b9dc02748b6a1b5931e9f629342
Asael211146 <91224136+Asael211146@users.noreply.github.com>
2025-11-11 02:47:52 -0600

    Refactor Administrador to extend Usuario class

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,3 +1,16 @@
-public class Administrador {
-    
+public class Administrador extends Usuario {
+
+    // Constructor
+    public Administrador(String nombre, String email) {
+        super(nombre, email);
+    }
+
+    // Métodos
+    public void publicarContenido(Contenido c) {
+        System.out.println("El administrador publicó el contenido: " + c._getTitulo());
+    }
+
+    public void eliminarContenido(Contenido c) {
+        System.out.println("El administrador eliminó el contenido: " + c._getTitulo());
+    }
 }
